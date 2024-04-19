7d9ac650893ea5d0e7f56d032b588d7fe4d8c5e9
Zaasth <wellintonpaypal@gmail.com>
2024-04-19 16:38:15 -0300

    commit

--- js/bind/app.js
+++ js/bind/app.js
@@ -17,3 +17,18 @@ const fun = (e) => {
 
 let filtrar = Peoples.filter(fun);
 console.log(filtrar);
+
+const conteudoBase = `
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{titulo}</title>
+</head>
+<body>
+    <h1>{titulo}</h1>
+    <p>Esta é a página {titulo}.</p>
+</body>
+</html>
+`;
